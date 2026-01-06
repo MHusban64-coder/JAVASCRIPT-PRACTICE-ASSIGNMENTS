29f458ecd01ab726a5cc352cf055f0bbe1d97ff4
MHusban <qasiraslam64@gmail.com>
2026-01-06 11:14:38 +0500

    show password function has been developed

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ function countFunction(event){
 
     count.innerText = countValue;
 }
+// completed
+// Second
+function checkPassword(){
+    var pass = document.getElementById("pass")
+ 
+    switch (pass.type) {
+        case "password":
+            pass.type="text"
+            break;
+    
+        default:
+            pass.type="password"
+            break;
+    }
+
+}
+// second also completed
